426edeb57ed81204447dca99dae4a6c03e9ddae9
Kelevra-pro <damtat8@gmail.com>
2019-11-19 20:07:14 +0300

    add layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Layout from './hoc/Layout/Layout';
+import classes from './App.module.scss';
+// import Quiz from "./containers/Quiz/Quiz";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+// console.log(classes);
+
+class App extends Component {
+    render() {
+        return (
+            <Layout>
+                <div className={classes.test}>
+                    <h1>Layout works</h1>
+                </div>
+
+                {/*<Quiz/>*/}
+            </Layout>
+        )
+    }
 }
 
 export default App;
