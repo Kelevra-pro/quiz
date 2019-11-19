f86368837f5698aae033687ca1b20dfeec19eb9a
Kelevra-pro <damirgolova@gmail.com>
2019-11-19 22:46:44 +0300

    first page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout';
 import classes from './App.module.scss';
-// import Quiz from "./containers/Quiz/Quiz";
+import Quiz from "./containers/Quiz/Quiz";
 
-// console.log(classes);
+console.log(classes);
 
 class App extends Component {
     render() {
         return (
             <Layout>
-                <div className={classes.test}>
-                    <h1>Layout works</h1>
-                </div>
-
-                {/*<Quiz/>*/}
+                <Quiz/>
             </Layout>
         )
     }
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import classes from './Quiz.module.scss'
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div>
+            <div className={classes.Quiz}>
+                <h1>Quiz</h1>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default Quiz
