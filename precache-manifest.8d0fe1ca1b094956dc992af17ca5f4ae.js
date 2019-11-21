b49c66c34abf25c3da066ab2f94d5dcc32081426
Kelevra-pro <damirgolova@gmail.com>
2019-11-21 20:10:04 +0300

    Updates

---
+++ precache-manifest.8d0fe1ca1b094956dc992af17ca5f4ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7de995db47b96067af032f7652a89f",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "1994e5df4746e0bdf92b",
+    "url": "/react-quiz/static/css/main.72823191.chunk.css"
+  },
+  {
+    "revision": "baf1fc3e603223e90495",
+    "url": "/react-quiz/static/js/2.58e39c33.chunk.js"
+  },
+  {
+    "revision": "1994e5df4746e0bdf92b",
+    "url": "/react-quiz/static/js/main.0761d445.chunk.js"
+  },
+  {
+    "revision": "23de2f01f24e601f99d6",
+    "url": "/react-quiz/static/js/runtime-main.9376759c.js"
+  }
+]);
