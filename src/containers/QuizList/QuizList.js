aa3257ffc1101b09095e199a7653beb3841b805a
Kelevra-pro <damirgolova@gmail.com>
2019-11-29 13:27:49 +0300

    add quiz list

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import classes from './QuizList.module.scss'
+// import index from "postcss-normalize";
+import {NavLink} from "react-router-dom";
+
+export default class QuizList extends Component {
+
+    renderQuizzes() {
+        return [1, 2, 3].map((quiz, index) => {
+            return (
+                <li
+                key={index}
+                >
+                    <NavLink to={'/quiz/' + quiz} >
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div className={classes.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        {this.renderQuizzes()}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
